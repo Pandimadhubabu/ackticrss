2ebcdab83cb2d0334b00f74313850d1dfcbb267f
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2020-03-23 22:50:13 +0530

    Update rss.js

--- rss.js
+++ rss.js
@@ -1,5 +1,5 @@
 var menu=[
 {id:"ABC",cat:"News",des:"ABC KFSN Local Fresno and National News.",uri:"https://news.google.com/rss?hl=en-IN&gl=IN&ceid=IN:en",ext:"https://news.google.com"},
-{id:"ABC",cat:"News",des:"ABC KFSN Local Fresno and National News.",uri:"https://feeds.feedburner.com/01/ar/a",ext:"https://feeds.feedburner.com"},
+{id:"ABC123",cat:"News",des:"ABC KFSN Local Fresno and National News.",uri:"https://feeds.feedburner.com/01/ar/a",ext:"https://feeds.feedburner.com"},
 {id:"NBC",cat:"News",des:"NBC KSEE YourCentralValley in Fresno, California.",uri:"https://news.google.com/rss?hl=en-US&gl=US&ceid=US:en",ext:"https://news.google.com"}
 ]
