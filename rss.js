53c81aad055b9d0d4e09d718c0905fecdfc73138
Madhu Babu <17622671+Pandimadhubabu@users.noreply.github.com>
2020-03-23 22:31:35 +0530

    Create rss.js

---
+++ rss.js
@@ -0,0 +1,4 @@
+var menu=[
+{id:"ABC",cat:"News",des:"ABC KFSN Local Fresno and National News.",uri:"http://news.google.com/rss?hl=en-IN&gl=IN&ceid=IN:en",ext:"https://abc30.com"},
+{id:"NBC",cat:"News",des:"NBC KSEE YourCentralValley in Fresno, California.",uri:"http://news.google.com/rss?hl=en-IN&gl=IN&ceid=IN:en",ext:"https://www.yourcentralvalley.com"},
+]
